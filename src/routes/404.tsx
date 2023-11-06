51db38b548a7c7336dab729743dcde6b89e7d8f5
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-06 18:34:53 +0100

    Add 404 page test

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,10 +1,25 @@
 import { component$ } from "@builder.io/qwik";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import ImgAliensAlien from "~/media/aliens-alien.gif?jsx";
 
 export default component$(() => {
   return (
     <>
-      <h1>OPSS!</h1>
+      <section class="inner-section">
+        <div class="flex flex-col gap-12 items-center justify-center">
+          <h1 class="mb-6">OPSS!</h1>
+          <p>
+            Uh-oh! It looks like you've wandered into the cosmic void of
+            cyberspace. Our binary aliens are throwing a rave just around the
+            corner. Hurry back before they invite you to dance in zeros and
+            ones! Alternatively, try our game at this{" "}
+            <Link href="../button-game">link</Link>.
+          </p>
+          {/* width="498"
+          height="280" */}
+          <ImgAliensAlien alt="Binary aliens" />
+        </div>
+      </section>
     </>
   );
 });
