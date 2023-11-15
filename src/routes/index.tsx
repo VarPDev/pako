e71df3af8c47efe89fe1781e2e298788f679d099
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-15 22:23:35 +0100

    Add transitions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,9 @@
-import { component$ } from "@builder.io/qwik";
+import {
+  Signal,
+  component$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead, Link } from "@builder.io/qwik-city";
 import { Hero } from "~/components/hero/hero";
 import { Cards } from "~/components/cards/cards";
@@ -12,6 +17,13 @@ import { projects } from "~/repository/projects";
 import { frontEnd, backEnd, tools } from "~/repository/stack";
 import { format } from "date-fns";
 import ImgPR from "/src/media/badge-first-pr.webp?jsx";
+
+interface Refs {
+  el: Signal<Element | undefined>;
+  isVisible: Signal<Boolean>;
+  observer: any;
+  countVisible: number;
+}
 
 function getLang(lang: Array<string>): string {
   switch (true) {
@@ -44,17 +56,174 @@ export const useArticles = routeLoader$(async (requestEvent) => {
 export default component$(() => {
   const articles = useArticles();
 
+  const projectTitleRef = useSignal<Element>();
+  const projectTitleIsVisible = useSignal<Boolean>(true);
+
+  const projectRef = useSignal<Element>();
+  const projectIsVisible = useSignal<Boolean>(true);
+
+  const historyTitleRef = useSignal<Element>();
+  const historyTitleIsVisible = useSignal<Boolean>(true);
+
+  const historyeRef = useSignal<Element>();
+  const historyIsVisible = useSignal<Boolean>(true);
+
+  const articlesTitleRef = useSignal<Element>();
+  const articlesTitleIsVisible = useSignal<Boolean>(true);
+
+  const articlesRef = useSignal<Element>();
+  const articlesIsVisible = useSignal<Boolean>(true);
+
+  const stackTitleRef = useSignal<Element>();
+  const stackTitleIsVisible = useSignal<Boolean>(true);
+
+  const stackFeRef = useSignal<Element>();
+  const stackFeIsVisible = useSignal<Boolean>(true);
+
+  const stackBeRef = useSignal<Element>();
+  const stackBeIsVisible = useSignal<Boolean>(true);
+
+  const stackToRef = useSignal<Element>();
+  const stackToIsVisible = useSignal<Boolean>(true);
+
+  const openTitleRef = useSignal<Element>();
+  const openTitleIsVisible = useSignal<Boolean>(true);
+
+  const openRef = useSignal<Element>();
+  const openIsVisible = useSignal<Boolean>(true);
+
+  const linksTitleRef = useSignal<Element>();
+  const linksTitleIsVisible = useSignal<Boolean>(true);
+
+  const linksRef = useSignal<Element>();
+  const linksIsVisible = useSignal<Boolean>(true);
+
+  const refs: Array<Refs> = [
+    {
+      el: projectTitleRef,
+      isVisible: projectTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: projectRef,
+      isVisible: projectIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: historyTitleRef,
+      isVisible: historyTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: historyeRef,
+      isVisible: historyIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: articlesTitleRef,
+      isVisible: articlesTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: articlesRef,
+      isVisible: articlesIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: stackTitleRef,
+      isVisible: stackTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: stackFeRef,
+      isVisible: stackFeIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: stackBeRef,
+      isVisible: stackBeIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: stackToRef,
+      isVisible: stackToIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: openTitleRef,
+      isVisible: openTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    { el: openRef, isVisible: openIsVisible, observer: null, countVisible: 0 },
+    {
+      el: linksTitleRef,
+      isVisible: linksTitleIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+    {
+      el: linksRef,
+      isVisible: linksIsVisible,
+      observer: null,
+      countVisible: 0,
+    },
+  ];
+
+  useVisibleTask$(() => {
+    refs.forEach((ref) => {
+      if (ref.el.value) {
+        // TODO: when isVisible became true remove observer
+        ref.observer = new IntersectionObserver(([entry]) => {
+          ref.isVisible.value = entry.isIntersecting;
+          ref.countVisible = entry.intersectionRatio
+            ? ref.countVisible + 1
+            : ref.countVisible;
+
+          console.log("🚀 ~ entry.isIntersecting:", entry.isIntersecting);
+          if (ref.isVisible.value && ref.countVisible > 0) {
+            ref.observer.disconnect();
+          }
+        });
+
+        ref.observer.observe(ref.el.value);
+      }
+    });
+  });
+
   return (
     <>
       <section>
         <Hero role={works[0].role} company={works[0].company} />
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={projectTitleRef}
+        class={`
+      title-section text-center
+      animation
+      ${projectTitleIsVisible.value && "isVisible"}
+    `}
+      >
         <h2>Recent projects</h2>
         {/* <h3>All my jobs</h3> */}
       </section>
-      <section class="inner-section">
+      <section
+        ref={projectRef}
+        class={`inner-section
+      animation
+      ${projectIsVisible.value && "isVisible"}`}
+      >
         <Cards items={projects} limit={3} />
 
         <p class="flex justify-center pt-6">
@@ -68,19 +237,42 @@ export default component$(() => {
         </p>
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={historyTitleRef}
+        class={`
+      title-section text-center
+      animation
+      ${historyTitleIsVisible.value && "isVisible"}
+    `}
+      >
         <h2>History</h2>
         <h3>All my jobs</h3>
       </section>
-      <section class="w-11/12 lg:w-5/6 sticky">
+      <section
+        ref={historyeRef}
+        class={`w-11/12 lg:w-5/6 sticky animation
+      ${historyIsVisible.value && "isVisible"}`}
+      >
         <Timeline items={works}></Timeline>
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={articlesTitleRef}
+        class={`
+      title-section text-center
+      animation
+      ${articlesTitleIsVisible.value && "isVisible"}
+    `}
+      >
         <h2>Recent articles</h2>
         {/* <h3>All my jobs</h3> */}
       </section>
-      <section class="inner-section">
+      <section
+        ref={articlesRef}
+        class={`inner-section
+      animation
+      ${articlesIsVisible.value && "isVisible"}`}
+      >
         <Articles articles={articles.value} limit={4} />
 
         <p class="flex justify-center pt-6">
@@ -94,31 +286,62 @@ export default component$(() => {
         </p>
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={stackTitleRef}
+        class={`title-section text-center
+      animation
+      ${stackTitleIsVisible.value && "isVisible"}`}
+      >
         <h2>Stack</h2>
         <h3>My tecnology stack</h3>
       </section>
+
       {/* front end */}
-      <section class="inner-section">
+      <section
+        ref={stackFeRef}
+        class={`inner-section
+        animation
+        ${stackFeIsVisible.value && "isVisible"}`}
+      >
         <Stacks title="Front end" stacks={frontEnd} />
       </section>
 
       {/* back end */}
-      <section class="inner-section">
+      <section
+        ref={stackBeRef}
+        class={`inner-section
+        animation
+        ${stackBeIsVisible.value && "isVisible"}`}
+      >
         <Stacks title="Back end" stacks={backEnd} />
       </section>
 
       {/* tools end */}
-      <section class="inner-section">
+      <section
+        ref={stackToRef}
+        class={`inner-section
+        animation
+        ${stackToIsVisible.value && "isVisible"}`}
+      >
         <Stacks title="Tools" stacks={tools} />
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={openTitleRef}
+        class={`title-section text-center
+      animation
+      ${openTitleIsVisible.value && "isVisible"}`}
+      >
         <h2>Open source world</h2>
         <h3>My first accepted PR | Qwik</h3>
       </section>
 
-      <section class="inner-section">
+      <section
+        ref={openRef}
+        class={`inner-section
+        animation
+        ${openIsVisible.value && "isVisible"}`}
+      >
         <div class="flex items-center justify-center">
           <ImgPR
             alt="My first PR"
@@ -127,11 +350,21 @@ export default component$(() => {
         </div>
       </section>
 
-      <section class="title-section text-center">
+      <section
+        ref={linksTitleRef}
+        class={`title-section text-center
+      animation
+      ${linksIsVisible.value && "isVisible"}`}
+      >
         <h2>Links</h2>
         <h3>Some of my socials</h3>
       </section>
-      <section class="link-section">
+      <section
+        ref={linksRef}
+        class={`link-section
+        animation
+        ${linksIsVisible.value && "isVisible"}`}
+      >
         <LinkItem links={links}></LinkItem>
       </section>
     </>
