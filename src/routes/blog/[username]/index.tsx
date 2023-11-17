81e1f862b7f75855197d7264a63eb5932101c4ec
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-17 14:37:21 +0100

    Try again

--- src/routes/blog/[username]/index.tsx
+++ src/routes/blog/[username]/index.tsx
@@ -1,6 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import { StaticGenerateHandler, useLocation } from "@builder.io/qwik-city";
 
+const getUser = async () => {
+  const res = await fetch("https://dev.to/api/users/1198163");
+  const user = await res.json();
+  return user.username;
+};
+
 export default component$(() => {
   const { params } = useLocation();
 
@@ -8,7 +14,9 @@ export default component$(() => {
 });
 
 export const onStaticGenerate: StaticGenerateHandler = async ({ env }) => {
+  const username = await getUser();
+
   return {
-    params: [{ username: "nyruchi" }],
+    params: [{ username }],
   };
 };
