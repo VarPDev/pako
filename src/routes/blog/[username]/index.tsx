b432cc09ccc6a459edb39f6e6cfa344a62307078
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-17 14:32:21 +0100

    Try again

--- src/routes/blog/[username]/index.tsx
+++ src/routes/blog/[username]/index.tsx
@@ -1,4 +1,11 @@
-import { StaticGenerateHandler } from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import { StaticGenerateHandler, useLocation } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  const { params } = useLocation();
+
+  return <p>Example: {params.username}</p>;
+});
 
 export const onStaticGenerate: StaticGenerateHandler = async ({ env }) => {
   return {
