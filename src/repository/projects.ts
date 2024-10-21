f637555afbe42c7adfd8437f1bf0981db1a01c7c
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2024-10-21 15:23:16 +0200

    Update projects.ts

--- src/repository/projects.ts
+++ src/repository/projects.ts
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    id: 1,
+    id: 6,
     title: 'Qwik',
     description: 'I joined this open source project as a contributor',
     altImage: 'Qwik logo picture',
@@ -11,7 +11,7 @@ export const projects = [
     href: 'https://qwik.builder.io/',
   },
   {
-    id: 2,
+    id: 5,
     title: 'PLMaker',
     description: 'Price list generator with some utilities',
     altImage: 'price list maker',
@@ -22,7 +22,7 @@ export const projects = [
     href: 'https://plmaker.dejizen.com/',
   },
   {
-    id: 2,
+    id: 4,
     title: 'Dejizen',
     description: 'A brand that build many things',
     altImage: 'dejizen',
@@ -45,7 +45,7 @@ export const projects = [
     href: 'https://rick-and-morty-e.web.app/',
   },
   {
-    id: 4,
+    id: 2,
     title: 'This website',
     description: 'Feel free to check out how I developed this website',
     altImage: 'Pasquale De Lucia picture',
@@ -56,7 +56,7 @@ export const projects = [
     href: 'https://github.com/VarPDev/pako',
   },
   {
-    id: 4,
+    id: 1,
     title: 'Resume me',
     description: 'An app built with Flutter to share your own resume',
     altImage: 'Resume me app logo',
@@ -65,17 +65,5 @@ export const projects = [
     type: 'Flutter',
     color: 'orange-600',
     href: 'https://apps.apple.com/it/app/resume-me/id1663622782',
-  },
-  {
-    id: 5,
-    title: 'Tab sync',
-    description:
-      'Npm library that allows you to communicate between multiple browser tabs',
-    altImage: 'Tab sync logo',
-    // image: "/rick_and_morty.png",
-    action: 'Try it',
-    type: 'NPM',
-    color: 'orange-600',
-    href: 'https://www.npmjs.com/package/@devhobby/tab-sync',
-  },
+  }
 ]
