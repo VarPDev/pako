a1c9dce7855aea49a4b39e0f17815a8488695502
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2024-11-20 08:43:39 +0100

    fix: dynamic sitemap

--- src/routes/dynamic-sitemap.xml/index.tsx
+++ src/routes/dynamic-sitemap.xml/index.tsx
@@ -1,11 +1,20 @@
 import type { RequestHandler } from '@builder.io/qwik-city'
 import { routes } from '@qwik-city-plan'
+import { pagesSlugsApi } from '~/services/graph-ql.service'
 import { createSitemap } from './create-sitemap'
 
-export const onGet: RequestHandler = ev => {
+export const onGet: RequestHandler = async ev => {
   const siteRoutes = routes
     .map(([route]) => route as string)
-    .filter(route => route !== '/') // Exclude the '/' route
+    .filter(route => route !== '/' && route !== 'finance/[slug]/') // Exclude the '/' route
+
+  const token = ev.env.get('DATO_CMS_TOKEN')
+  const allFinancePages = await pagesSlugsApi(token ?? '')
+
+  const slugs = allFinancePages.data.allPages.map(
+    (item: any) => `finance/${item.slug}/`,
+  )
+  siteRoutes.push(...slugs)
 
   const sitemap = createSitemap([
     { loc: '/', priority: 1 }, // Manually include the root route
