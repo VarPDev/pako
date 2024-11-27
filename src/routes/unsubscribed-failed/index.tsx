39c957724188f3b26bdfb4845785e5a9ce0ea91b
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2024-11-27 16:19:21 +0100

    feat: improve unsubscribe page

--- src/routes/unsubscribed-failed/index.tsx
+++ src/routes/unsubscribed-failed/index.tsx
@@ -1,6 +1,8 @@
 import { component$ } from '@builder.io/qwik'
 import { type DocumentHead } from '@builder.io/qwik-city'
 import { InnerSectionComponent } from '~/components/inner-section/innerSectionComponent'
+import { LinkItem } from '~/components/linkItem/linkItem'
+import { links } from '~/repository/links'
 
 export default component$(() => {
   return (
@@ -9,15 +11,26 @@ export default component$(() => {
         <h2 class="font-bold uppercase mt-12 mb-16">Something wrong</h2>
       </section>
       <InnerSectionComponent showCta={false}>
-        <p class="text-xl">
+        <p class="text-xl text-center">
           Write to{' '}
-          <a href="mailto:pasquale.delucia96@gmail.com">
+          <a class="text-primary" href="mailto:pasquale.delucia96@gmail.com">
             pasquale.delucia96@gmail.com
           </a>{' '}
           to Unsubscribe.
         </p>
-        <p class="text-xl">Remember to send email you want to unsubscribe</p>
+        <p class="text-xl text-center">
+          Remember to send email you want to unsubscribe
+        </p>
       </InnerSectionComponent>
+
+      <section id="links" class="title-section text-center">
+        <h2>Links</h2>
+        <p class="text-xl">Stay updated or get in touch on my socials</p>
+      </section>
+
+      <section class="link-section">
+        <LinkItem links={links} referrer="index"></LinkItem>
+      </section>
     </>
   )
 })
--- src/routes/unsubscribed/index.tsx
+++ src/routes/unsubscribed/index.tsx
@@ -1,21 +1,32 @@
 import { component$ } from '@builder.io/qwik'
 import { type DocumentHead } from '@builder.io/qwik-city'
 import { InnerSectionComponent } from '~/components/inner-section/innerSectionComponent'
+import { LinkItem } from '~/components/linkItem/linkItem'
+import { links } from '~/repository/links'
 
 export default component$(() => {
   return (
     <>
       <section class="title-section text-center">
-        <h2 class="font-bold uppercase mt-12 mb-16">Unsubscribe Successful</h2>
+        <h2 class="font-bold uppercase my-12">Unsubscribe Successful</h2>
       </section>
       <InnerSectionComponent showCta={false}>
-        <p class="text-xl">
+        <p class="text-xl text-center">
           You have successfully unsubscribed. We're sorry to see you go!
         </p>
-        <p class="text-xl">
+        <p class="text-xl text-center">
           If this was a mistake, feel free to subscribe again anytime.
         </p>
       </InnerSectionComponent>
+
+      <section id="links" class="title-section text-center">
+        <h2>Links</h2>
+        <p class="text-xl">Stay updated or get in touch on my socials</p>
+      </section>
+
+      <section class="link-section">
+        <LinkItem links={links} referrer="index"></LinkItem>
+      </section>
     </>
   )
 })
