5f9c4ac9613c3a648e7951c7dcc773fc71f94f23
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2025-06-05 09:46:06 +0200

    fix: add easter egss also in blog layout

--- src/routes/layout-blog.tsx
+++ src/routes/layout-blog.tsx
@@ -10,13 +10,19 @@ import {
   type RequestHandler,
 } from '@builder.io/qwik-city'
 import { InitialValues } from '@modular-forms/qwik'
+import { isWithinInterval, set } from 'date-fns'
 import { AnimatedComp } from '~/components/animated-component/animated-component'
 import { CommentForm } from '~/components/blog/blogComponent'
+import { Cat } from '~/components/cat/cat'
+import { CatWalk } from '~/components/cat/cat-walk'
+import { Eggs } from '~/components/eggs/eggs'
 import { Footer } from '~/components/footer/footer'
 import { Header } from '~/components/header/header'
 import { InnerSectionComponent } from '~/components/inner-section/innerSectionComponent'
 import { Newsletter, NewsletterForm } from '~/components/newsletter/newsletter'
-import { ShowContext } from '~/services/common.service'
+import { Rudolph } from '~/components/rudolph/rudolph'
+import { Santa } from '~/components/santa/santa'
+import { ShowContext, SnowContext } from '~/services/common.service'
 
 export const useNewsFormLoader = routeLoader$<InitialValues<NewsletterForm>>(
   () => ({
@@ -50,10 +56,58 @@ export default component$(() => {
   const match = location.url.pathname.match(/^\/[^/]+\/([^/]+)\/?/)
   const blogType = match ? match[1] : 'dev'
 
+  const today = new Date()
+  const snowStart = set(today, {
+    month: 11,
+    date: 5,
+  })
+  const snowEnd = set(today, {
+    year: today.getFullYear() + 1,
+    month: 0,
+    date: 15,
+  })
+
+  const snowInRange = isWithinInterval(today, {
+    start: snowStart,
+    end: snowEnd,
+  })
+
+  const snows = useSignal(snowInRange ? new Array(200).fill(0) : null)
+  useContextProvider(SnowContext, snows)
+
+  const easterStart = set(today, {
+    month: 2,
+    date: 5,
+  })
+
+  const easterEnd = set(today, {
+    month: 4,
+    date: 1,
+  })
+
+  const easterInRange = isWithinInterval(today, {
+    start: easterStart,
+    end: easterEnd,
+  })
+
   return (
     <>
+      {show.value && snows.value && (
+        <div class="snow-container">
+          {snows.value.map((item, index) => (
+            <div key={index} class="snow"></div>
+          ))}
+        </div>
+      )}
+
       <Header />
       <main>
+        {show.value && !snows.value && !easterInRange && <Cat />}
+        {/* If I move this after the Slot, on value change slot reload */}
+        {show.value && !snows.value && !easterInRange && <CatWalk />}
+        {show.value && snows.value && <Santa />}
+        {show.value && snows.value && <Rudolph />}
+        {show.value && easterInRange && <Eggs />}
         <Slot />
 
         <AnimatedComp>
